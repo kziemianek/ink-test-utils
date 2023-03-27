d38f03023d8abcefce0794e4c62bb46eef72b55b
Kasper Ziemianek <kasper.ziemianek@gmail.com>
2023-03-27 23:24:51 +0200

    fix docs

--- src/lib.rs
+++ src/lib.rs
@@ -11,16 +11,46 @@ mod assert_events_order;
 
 /// Invoke provided code for selected events
 ///
-/// # Usage
-///
-/// ```
-/// <event_idx>: <event_type> (<event_fields>) {
-///     <code>,
-/// }
-/// ```
-///
 /// # Example
-/// ```
+///```
+/// # #![cfg_attr(not(feature = "std"), no_std)]
+/// # #[ink::contract]
+/// # mod foobar {
+/// #
+/// #   #[ink(storage)]
+/// #   pub struct Foobar {}
+/// #
+/// #   #[ink(event)]
+/// #   pub struct FoobarHappened {
+/// #       index: u16,
+/// #       len: u16,
+/// #   }
+/// #
+/// #   impl Foobar {
+/// #       #[ink(constructor)]
+/// #       pub fn new() -> Self {
+/// #           Self {}
+/// #       }
+/// #
+/// #       #[ink(message)]
+/// #       pub fn foobar(&self) {
+/// #           self.env().emit_event(FoobarHappened { index: 2, len: 4 });
+/// #       }
+/// #   }
+/// #
+/// #   #[cfg(test)]
+/// #   mod tests {
+/// #       use super::*;
+/// #       use crate::foobar::Foobar;
+/// #       use ink_test_utils::assert_event;
+/// #
+/// #       type Event = <Foobar as ::ink::reflect::ContractEventBase>::Type;
+/// #
+/// #       #[ink::test]
+/// #       pub fn test_single_assertion() {
+/// #           let expected_index: u16 = 2;
+/// #           let foobar = Foobar::new();
+/// #           foobar.foobar();
 /// assert_event! {
 ///     0: FoobarHappened (index) [
 ///         assert_eq!(
@@ -29,6 +59,9 @@ mod assert_events_order;
 ///         ),
 ///     ]
 /// }
+/// #       }
+/// #   }
+/// # }
 /// ```
 #[proc_macro]
 pub fn assert_event(input: TokenStream) -> TokenStream {
@@ -39,7 +72,44 @@ pub fn assert_event(input: TokenStream) -> TokenStream {
 ///
 /// # Example
 /// ```
+/// # #![cfg_attr(not(feature = "std"), no_std)]
+/// #
+/// # #[ink::contract]
+/// # mod foobar {
+/// #
+/// #     #[ink(storage)]
+/// #     pub struct Foobar {}
+/// #
+/// #     #[ink(event)]
+/// #     pub struct FoobarHappened {
+/// #         index: u16,
+/// #     }
+/// #
+/// #     impl Foobar {
+/// #         #[ink(constructor)]
+/// #         pub fn new() -> Self {
+/// #             Self {}
+/// #         }
+/// #
+/// #         #[ink(message)]
+/// #         pub fn foobar(&self) {
+/// #             self.env().emit_event(FoobarHappened { index: 2 });
+/// #         }
+/// #     }
+/// #
+/// #     #[cfg(test)]
+/// #     mod tests {
+/// #         use crate::foobar::Foobar;
+/// #         use ink_test_utils::assert_events_len;
+/// #
+/// #         #[ink::test]
+/// #         pub fn test() {
+/// #             let foobar = Foobar::new();
+/// #             foobar.foobar();
 /// assert_events_len!(1)
+/// #         }
+/// #     }
+/// # }
 /// ```
 #[proc_macro]
 pub fn assert_events_len(input: TokenStream) -> TokenStream {
@@ -50,9 +120,51 @@ pub fn assert_events_len(input: TokenStream) -> TokenStream {
 ///
 /// # Example
 /// ```
-///assert_events_order! {
+/// # #![cfg_attr(not(feature = "std"), no_std)]
+/// #
+/// # #[ink::contract]
+/// # mod foobar {
+/// #
+/// #     #[ink(storage)]
+/// #     pub struct Foobar {}
+/// #
+/// #     #[ink(event)]
+/// #     pub struct FoobarHappened {}
+/// #
+/// #     #[ink(event)]
+/// #     pub struct BarfooHappened {}
+/// #
+/// #     impl Foobar {
+/// #         #[ink(constructor)]
+/// #         pub fn new() -> Self {
+/// #             Self {}
+/// #         }
+/// #
+/// #         #[ink(message)]
+/// #         pub fn foobar(&self) {
+/// #             self.env().emit_event(FoobarHappened {});
+/// #             self.env().emit_event(BarfooHappened {});
+/// #         }
+/// #     }
+/// #
+/// #     #[cfg(test)]
+/// #     mod tests {
+/// #         use super::*;
+/// #         use crate::foobar::Foobar;
+/// #         use ink_test_utils::assert_events_order;
+/// #
+/// #         type Event = <Foobar as ::ink::reflect::ContractEventBase>::Type;
+/// #
+/// #         #[ink::test]
+/// #         pub fn test_single_assertion() {
+/// #             let foobar = Foobar::new();
+/// #             foobar.foobar();
+/// assert_events_order! {
 ///     [FoobarHappened, BarfooHappened]
-///}
+/// }
+/// #         }
+/// #     }
+/// # }
 /// ```
 #[proc_macro]
 pub fn assert_events_order(input: TokenStream) -> TokenStream {
